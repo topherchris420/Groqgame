74cd341081abd7f363b330eecf13a270bd263bff
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-16 13:04:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import time
 import random
 
 # Game constants
-GRID_SIZE = 20
+GRID_SIZE = 10
 INITIAL_SPEED = 0.2  # Initial snake movement speed (lower is faster)
 SNAKE_CHAR = '<div style="color: orange;">Powered by Groq</div>'
-FOOD_CHAR = '<div style="color: red;">Vers3Dynamics</div>'
-GROQ_CHAR = '<div style="color: black;">■</div>'
+FOOD_CHAR = '<div style="color: green;">Vers3Dynamics</div>'
+GROQ_CHAR = '<div style="color: purple;">by christopher■</div>'
 POWER_UP_CHAR = '<div style="color: green;">■</div>'
 
 # Streamlit components
